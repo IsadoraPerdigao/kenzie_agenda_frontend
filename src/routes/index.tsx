2b2abad02078a328aafdff6b9c5772256d48ace6
Isadora <dorisperdigao@gmail.com>
2023-05-25 18:38:15 -0300

    initial commit

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom"
+
+export const RoutesMain = () => {
+    return (
+        <Routes>
+            <Route path="/" />
+            <Route path="/register" />
+            <Route path="/dashboard" />
+        </Routes>
+    )
+}
